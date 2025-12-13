0618fbfc147b3b60773ec44b57a69d9402051c9e
Your Name <you@example.com>
2025-12-13 09:51:12 +0800

    lavender: fix header race conditions and missing qcom symbols
    * Create local media_shim with generated kernel headers (media/drm)
    * Point camera and display HALs to shim instead of raw KERNEL_OBJ
    * Fixes 'sigaction' redefinition and missing 'msmb_isp.h' errors

--- camera/common.mk
+++ camera/common.mk
@@ -3,5 +3,10 @@ kernel_includes :=
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+#   kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+# COMMENT OUT THE BAD LINE:
+# kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+# ADD YOUR SHIM PATH INSTEAD:
+kernel_includes += device/xiaomi/lavender/camera/include/media_shim
 endif
